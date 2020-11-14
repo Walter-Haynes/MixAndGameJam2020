e4f349a09204a1a2a96002fe117953f1388557aa
Walter Haynes <walter-haynes@outlook.com>
2020-11-14 16:58:50 +0100

    Separated Side Jumps

--- Assets/-Source-/Scripts/Game/Player/Movement/PlayerWalk.cs
+++ Assets/-Source-/Scripts/Game/Player/Movement/PlayerWalk.cs
@@ -18,12 +18,12 @@ namespace Scripts.Game.Player.Movement
 
 		[SerializeField] private float hopDuration = 0.05f;
 
-		[SerializeField] private float jumpHopDuration = 0.5f;
-		
 		[UsedImplicitly]
 		private bool _wannaWalkLeft = false, _wannaWalkRight = false;
 		[UsedImplicitly]
 		private bool _wannaJump = false;
+
+		private bool WannaWalk => (_wannaWalkLeft || _wannaWalkRight);
 		
 		#endregion
 
@@ -39,13 +39,12 @@ namespace Scripts.Game.Player.Movement
 
 		internal override void AbilityFixedUpdate()
 		{
-			if(_wannaWalkRight)
+			if (!_wannaJump)
 			{
-				TryWalk(left: false);
-			}
-			else if(_wannaWalkLeft)
-			{
-				TryWalk(left: true);
+				if(WannaWalk)
+				{
+					TryWalk();
+				}
 			}
 
 			//reset
@@ -55,7 +54,7 @@ namespace Scripts.Game.Player.Movement
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		private bool TryWalk(bool left)
+		private bool TryWalk()
 		{
 			//TODO: (Walter) Beat check.
 
@@ -63,59 +62,23 @@ namespace Scripts.Game.Player.Movement
 			
 			Vector3 __startPos = transform.localPosition;
 			
-			if (Player.Inputs.Jump.phase == InputActionPhase.Started)
+			if (_wannaWalkLeft)
 			{
-				__JumpHop();	
+
+				transform.localPositionTransition_X(position: __startPos.x - 1, duration: hopDuration);
 			}
 			else
 			{
-				__WalkHop();	
+				transform.localPositionTransition_X(position: __startPos.x + 1, duration: hopDuration);
 			}
 			
+			//hop
+			transform
+				.localPositionTransition_Y(position: __startPos.y + 0.25f, duration: hopDuration / 2.0f, ease: LeanEase.SineOut)
+				.JoinTransition()
+				.localPositionTransition_Y(position: __startPos.y,         duration: hopDuration / 2.0f, ease: LeanEase.SineOut);
+
 			return true;
-
-			void __WalkHop()
-			{
-				if (left)
-				{
-
-					transform.localPositionTransition_X(position: __startPos.x - 1, duration: hopDuration);
-				}
-				else
-				{
-					transform.localPositionTransition_X(position: __startPos.x + 1, duration: hopDuration);
-				}
-			
-				//hop
-				transform
-					.localPositionTransition_Y(position: __startPos.y + 0.25f, duration: hopDuration / 2.0f, ease: LeanEase.SineOut)
-					.JoinTransition()
-					.localPositionTransition_Y(position: __startPos.y,         duration: hopDuration / 2.0f, ease: LeanEase.SineOut);
-			}
-			
-			void __JumpHop()
-			{
-				if (left)
-				{
-
-					transform.localPositionTransition_X(position: __startPos.x - 3, duration: jumpHopDuration);
-				}
-				else
-				{
-					transform.localPositionTransition_X(position: __startPos.x + 3, duration: jumpHopDuration);
-				}
-			
-				//hop
-				transform
-					.localPositionTransition_Y(position: __startPos.y + 2, duration: jumpHopDuration / 2.0f, ease: LeanEase.SineOut)
-					.JoinTransition()
-					.localPositionTransition_Y(position: __startPos.y,     duration: jumpHopDuration / 2.0f, ease: LeanEase.SineOut);
-			}
-		}
-
-		private bool TryJumpHop()
-		{
-			
 		}
 
 		#endregion
