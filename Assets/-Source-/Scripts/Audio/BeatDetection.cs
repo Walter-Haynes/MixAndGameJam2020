0c023682eb25609861acb8f6fd8a200001b9b015
Sam <samyam.utube@gmail.com>
2020-11-14 11:07:29 -0500

    Fire events on every full, half, and quarter beat

--- Assets/-Source-/Scripts/Audio/BeatDetection.cs
+++ Assets/-Source-/Scripts/Audio/BeatDetection.cs
@@ -3,6 +3,15 @@
 /// </summary>
 public class BeatDetection : AudioSyncer
 {
+    # region Delegates
+        public delegate void FullBeat(float val); 
+        private FullBeat fullBeatDelegate;
+        public delegate void HalfBeat(float val); 
+        private HalfBeat halfBeatDelegate;
+        public delegate void QuarterBeat(float val); 
+        private QuarterBeat quarterBeatDelegate;
+    #endregion Delegates
+
     public override void OnUpdate() {
         base.OnUpdate();
 
@@ -11,11 +20,16 @@ public class BeatDetection : AudioSyncer
 
     public override void OnBeat(float val, BEAT_TYPE beatType) {
         base.OnBeat(val, beatType);
-
-        // Threshold
-        if (val >= bias + (bias * .5)) {
-        }
-        else {
+        switch(beatType) {
+            case BEAT_TYPE.FULL:
+                if (fullBeatDelegate != null) fullBeatDelegate(val);
+                break;
+            case BEAT_TYPE.HALF:
+                if (fullBeatDelegate != null) halfBeatDelegate(val);
+                break;
+            case BEAT_TYPE.QUARTER:
+                if (fullBeatDelegate != null) quarterBeatDelegate(val);
+                break;
         }
     }
 }
