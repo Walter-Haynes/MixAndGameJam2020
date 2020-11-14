635dced4a647dccc9126e86fc983b188a57c44ca
Walter Haynes <walter-haynes@outlook.com>
2020-11-14 11:40:49 +0100

    Added Utilities
    + MonoBehaviour Singleton
    + ScriptableObject Singleton

---
+++ Assets/-Source-/Scripts/Utilities/MathUtils.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Scripts.Utilities
+{
+	public static partial class MathUtils
+	{
+		[PublicAPI]
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float Abs(this float value) => Mathf.Abs(value);
+	}
+}
---
+++ Assets/-Source-/Scripts/Utilities/SingletonMonoBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using JetBrains.Annotations;
+
+namespace Scripts.Utilities
+{
+	/// <summary>
+	/// Lazy Singleton based on this: https://blog.mzikmund.com/2019/01/a-modern-singleton-in-unity/
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public abstract class SingletonMonoBehaviour<T> : MonoBehaviour 
+		where T : SingletonMonoBehaviour<T>
+	{
+		private static readonly Lazy<T> LazyInstance = new Lazy<T>(CreateSingleton);
+
+		[PublicAPI]
+		public static T Instance => LazyInstance.Value;
+
+		private static T CreateSingleton()
+		{
+			GameObject __ownerObject = new GameObject(name: $"[{typeof(T).Name}]");
+			T __instance = __ownerObject.AddComponent<T>();
+			DontDestroyOnLoad(__ownerObject);
+			return __instance;
+		}
+	}
+}
