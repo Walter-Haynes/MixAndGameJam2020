692f4bb834270e8bf950adc0e5e2926172d99937
Walter Haynes <walter-haynes@outlook.com>
2020-11-15 15:35:31 +0100

    Player Health

--- Assets/-Source-/Scripts/Game/Enemies/Bullet.cs
+++ Assets/-Source-/Scripts/Game/Enemies/Bullet.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
 	[Tooltip("Speed in PER BEAT values")]
-	[SerializeField] private float speed = 20;
+	[SerializeField] private float speed = 4;
 
 	private void Update()
 	{
 		float __speedPerSecond = speed * TempBeatThing.Instance.PerBeatToPerSecond;
 		float __speedPerFrame = __speedPerSecond * Time.deltaTime;
+		
+		Debug.Log($"\n Move Speed Per Beat = {speed} \n Move Speed Per Second = {__speedPerSecond}");
 		
 		Vector3 __translation = new Vector3(0, y: __speedPerFrame,0);
 
